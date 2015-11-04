662d3b4cd03e1e4a6eecfc3be87b2da273a69fce
Luca Tumedei <luca@theaveragedev.com>
2015-11-04 19:27:29 +0100

    added more Filesystem tests

--- tests/functional/tad/FrontToBack/Templates/FilesystemTest.php
+++ tests/functional/tad/FrontToBack/Templates/FilesystemTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace tad\FrontToBack\Templates;
 
+use org\bovigo\vfs\vfsStream;
 use tad\FunctionMocker\FunctionMocker as Test;
 
 class FilesystemTest extends \WP_UnitTestCase {
@@ -208,79 +209,79 @@ class FilesystemTest extends \WP_UnitTestCase {
 		$credentials->wasCalledWithOnce( [ get_current_user_id(), $creds ], 'set_for_user' );
 	}
 
-//	/**
-//	 * @test
-//	 * it should missing master template from templates folder
-//	 */
-//	public function it_should_spot_missing_master_template_from_templates_folder() {
-//		vfsStream::setup( 'root', null, [
-//			'wordpress' => [ 'content' => [ 'templates' ] ]
-//		] );
-//		$templates_root_folder = vfsStream::url( 'root/wordpress/content/templates' );
-//
-//		require_once( ABSPATH . 'wp-admin/includes/class-wp-filesystem-base.php' );
-//		$wpfs = Test::replace( 'WP_Filesystem_Base' )->method( 'exists', false )->get();
-//		$sut  = new Filesystem( $templates_root_folder, $wpfs );
-//
-//		$sut->initialize_wp_filesystem();
-//		$this->assertEquals( false, $sut->ensure_master_template() );
-//	}
-//
-//	/**
-//	 * @test
-//	 * it should duplicate the master template given a post name
-//	 */
-//	public function it_should_duplicate_the_master_template_given_a_post_name() {
-//		vfsStream::setup( 'root', null, [
-//			'wordpress' => [ 'content' => [ 'templates' => [ $this->masterTemplateName => '// some content' ] ] ]
-//		] );
-//		$root = vfsStream::url( 'root/wordpress/content/templates' );
-//
-//		$sut = new Filesystem( $root );
-//		$sut->duplicate_master_template( 'foo' );
-//
-//		$this->assertFileExists( $root . '/foo.' . $this->templatesExtension );
-//	}
-//
-//	/**
-//	 * @test
-//	 * it should not duplicate the master template again if already done
-//	 */
-//	public function it_should_not_duplicate_the_master_template_again_if_already_done() {
-//		vfsStream::setup( 'root', null, [
-//			'wordpress' => [
-//				'content' => [
-//					'templates' => [
-//						$this->masterTemplateName          => '// foo',
-//						'foo.' . $this->templatesExtension => '// bar'
-//					]
-//				]
-//			]
-//		] );
-//		$root = vfsStream::url( 'root/wordpress/content/templates' );
-//
-//		$sut = new Filesystem( $root );
-//		$sut->duplicate_master_template( 'foo' );
-//
-//		$file = $root . '/foo.' . $this->templatesExtension;
-//		$this->assertFileExists( $file );
-//		$this->assertEquals( '// bar', file_get_contents( $file ) );
-//	}
-//
-//	/**
-//	 * @test
-//	 * it should move templates
-//	 */
-//	public function it_should_move_templates() {
-//		$contents = 'foo file';
-//		vfsStream::setup( 'root', null, [ 'templates' => [ 'foo.php' => $contents ] ] );
-//		$root = vfsStream::url( 'root/templates' );
-//		$sut  = new Filesystem( $root );
-//
-//		$sut->move_template( 'foo', 'bar' );
-//
-//		$this->assertFileNotExists( $root . '/foo.php' );
-//		$this->assertFileExists( $root . '/bar.php' );
-//		$this->assertEquals( $contents, file_get_contents( $root . '/bar.php' ) );
-//	}
+	/**
+	 * @test
+	 * it should missing master template from templates folder
+	 */
+	public function it_should_spot_missing_master_template_from_templates_folder() {
+		vfsStream::setup( 'root', null, [
+			'wordpress' => [ 'content' => [ 'templates' ] ]
+		] );
+		$templates_root_folder = vfsStream::url( 'root/wordpress/content/templates' );
+
+		require_once( ABSPATH . 'wp-admin/includes/class-wp-filesystem-base.php' );
+		$wpfs = Test::replace( 'WP_Filesystem_Base' )->method( 'exists', false )->get();
+		$sut  = new Filesystem( $templates_root_folder, $wpfs );
+
+		$sut->initialize_wp_filesystem();
+		$this->assertEquals( false, $sut->ensure_master_template() );
+	}
+
+	/**
+	 * @test
+	 * it should duplicate the master template given a post name
+	 */
+	public function it_should_duplicate_the_master_template_given_a_post_name() {
+		vfsStream::setup( 'root', null, [
+			'wordpress' => [ 'content' => [ 'templates' => [ $this->masterTemplateName => '// some content' ] ] ]
+		] );
+		$root = vfsStream::url( 'root/wordpress/content/templates' );
+
+		$sut = new Filesystem( $root );
+		$sut->duplicate_master_template( 'foo' );
+
+		$this->assertFileExists( $root . '/foo.' . $this->templatesExtension );
+	}
+
+	/**
+	 * @test
+	 * it should not duplicate the master template again if already done
+	 */
+	public function it_should_not_duplicate_the_master_template_again_if_already_done() {
+		vfsStream::setup( 'root', null, [
+			'wordpress' => [
+				'content' => [
+					'templates' => [
+						$this->masterTemplateName          => '// foo',
+						'foo.' . $this->templatesExtension => '// bar'
+					]
+				]
+			]
+		] );
+		$root = vfsStream::url( 'root/wordpress/content/templates' );
+
+		$sut = new Filesystem( $root );
+		$sut->duplicate_master_template( 'foo' );
+
+		$file = $root . '/foo.' . $this->templatesExtension;
+		$this->assertFileExists( $file );
+		$this->assertEquals( '// bar', file_get_contents( $file ) );
+	}
+
+	/**
+	 * @test
+	 * it should move templates
+	 */
+	public function it_should_move_templates() {
+		$contents = 'foo file';
+		vfsStream::setup( 'root', null, [ 'templates' => [ 'foo.php' => $contents ] ] );
+		$root = vfsStream::url( 'root/templates' );
+		$sut  = new Filesystem( $root );
+
+		$sut->move_template( 'foo', 'bar' );
+
+		$this->assertFileNotExists( $root . '/foo.php' );
+		$this->assertFileExists( $root . '/bar.php' );
+		$this->assertEquals( $contents, file_get_contents( $root . '/bar.php' ) );
+	}
 }
