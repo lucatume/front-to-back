f4eb518a315d0e7b5113ffeb9a6d5304a10c254e
Luca Tumedei <luca@theaveragedev.com>
2016-03-09 13:54:06 +0100

    added the xml_strcasecmp function

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -6,3 +6,16 @@ use Codeception\Util\Autoload;
 
 FunctionMocker::init();
 Autoload::addNamespace( 'FTB\Test', codecept_data_dir( 'classes' ) );
+
+// utility functions
+function xml_strcasecmp( $xmlStringOne, $xmlStringTwo ) {
+	$pattern      = '/(?<=\\>)[\\s\\t\\n\\r]+(?=\\<)/';
+	$xmlStringOne = trim( preg_replace( $pattern, '', $xmlStringOne ) );
+	$xmlStringTwo = trim( preg_replace( $pattern, '', $xmlStringTwo ) );
+
+	$pattern      = '/[\\s\\t\\n\\r]+/';
+	$xmlStringOne = trim( preg_replace( $pattern, ' ', $xmlStringOne ) );
+	$xmlStringTwo = trim( preg_replace( $pattern, ' ', $xmlStringTwo ) );
+
+	return strcasecmp( $xmlStringOne, $xmlStringTwo ) === 0 ? true : false;
+}
