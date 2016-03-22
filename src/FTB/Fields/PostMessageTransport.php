c61b990674712e4335e07ad85dd06ea4ec60250e
Luca Tumedei <luca@theaveragedev.com>
2016-03-22 19:22:42 +0100

    added more tests

--- src/FTB/Fields/PostMessageTransport.php
+++ src/FTB/Fields/PostMessageTransport.php
@@ -8,6 +8,16 @@ class FTB_Fields_PostMessageTransport implements FTB_Fields_TransportInterface {
 	 */
 	protected $wrapping_tag = 'span';
 
+	/**
+	 * @var array
+	 */
+	protected $supported_tags = false;
+
+	/**
+	 * @var array
+	 */
+	protected $markup_mods = false;
+
 	public function add_field_args( $tag, array $field_args ) {
 		if ( ! in_array( $tag, $this->supported_tags() ) ) {
 			return $field_args;
@@ -46,6 +56,10 @@ class FTB_Fields_PostMessageTransport implements FTB_Fields_TransportInterface {
 	}
 
 	protected function supported_tags() {
+		if ( false !== $this->supported_tags ) {
+			return $this->supported_tags;
+		}
+
 		return array( 'title' );
 	}
 
@@ -59,7 +73,7 @@ class FTB_Fields_PostMessageTransport implements FTB_Fields_TransportInterface {
 
 	public function modify_output( $tag, array $field_args, $output ) {
 		if ( ! in_array( $tag, $this->supported_tags() ) ) {
-			return $field_args;
+			return $output;
 		}
 
 		return $this->modify_markup( $tag, $field_args, $output );
@@ -74,16 +88,20 @@ class FTB_Fields_PostMessageTransport implements FTB_Fields_TransportInterface {
 		if ( isset( $markup_mods[ $tag ]['callback'] ) ) {
 			return call_user_func( $markup_mods[ $tag ]['callback'], $tag, $field_args, $output );
 		} else {
-			return $markup_mods[ $tag ]['before'] . $output . [ $markup_mods ]['after'];
+			return $markup_mods[ $tag ]['before'] . $output . $markup_mods[ $tag ]['after'];
 		}
 	}
 
 	protected function markup_mods() {
+		if ( false !== $this->markup_mods ) {
+			return $this->markup_mods;
+		}
+
 		return array();
 	}
 
-	protected function wrap( $tag, array $field_args, $output ) {
-		return sprintf( '<span class=".%s" style="display:inline;">%s</span>', $field_args['settings'], $output );
+	public function set_supported_tags( array $supported_tags ) {
+		$this->supported_tags = $supported_tags;
 	}
 
 	protected function get_prefix( $tag, $field_args ) {
@@ -98,7 +116,7 @@ class FTB_Fields_PostMessageTransport implements FTB_Fields_TransportInterface {
 		return isset( $tag_suffixes[ $tag ] ) ? ftb_template( $tag_suffixes[ $tag ], $field_args ) : '';
 	}
 
-	private function tag_prefixes() {
+	protected function tag_prefixes() {
 		return array(
 			'title' => '<' . $this->wrapping_tag() . ' class=".{{settings}}" style="display: inline;">',
 		);
@@ -111,10 +129,14 @@ class FTB_Fields_PostMessageTransport implements FTB_Fields_TransportInterface {
 	}
 
 	protected function wrapping_tag() {
-		return 'span';
+		return $this->wrapping_tag;
 	}
 
 	public function set_wrapping_tag( $wrapping_tag ) {
 		$this->wrapping_tag = $wrapping_tag;
+	}
+
+	public function set_markup_mods( array  $markup_mods ) {
+		$this->markup_mods = $markup_mods;
 	}
 }
--- tests/wpunit/FTB/Fields/PostMessageTransportTest.php
+++ tests/wpunit/FTB/Fields/PostMessageTransportTest.php
@@ -29,6 +29,82 @@ class PostMessageTransportTest extends RefreshTransportTest {
 		$this->assertInstanceOf( 'FTB_Fields_PostMessageTransport', $sut );
 	}
 
+	/**
+	 * @test
+	 * it should return field args if tag not supported
+	 */
+	public function it_should_return_field_args_if_tag_not_supported() {
+		$field_args = [ 'settings' => 'value' ];
+		$sut        = $this->make_instance();
+
+		$this->assertEquals( $field_args, $sut->add_field_args( 'unsupported', $field_args ) );
+	}
+
+	/**
+	 * @test
+	 * it should not add function if tag requires no function
+	 */
+	public function it_should_not_add_function_if_tag_requires_no_function() {
+		$field_args = [ 'settings' => 'value' ];
+		$sut        = $this->make_instance();
+		$sut->set_supported_tags( [ 'some-type' ] );
+
+		$this->assertArrayNotHasKey( 'function', $sut->add_field_args( 'some-type', $field_args )['js_vars'] );
+	}
+
+	/**
+	 * @test
+	 * it should not add property if tag requires no property
+	 */
+	public function it_should_not_add_property_if_tag_requires_no_property() {
+		$field_args = [ 'settings' => 'value' ];
+		$sut        = $this->make_instance();
+		$sut->set_supported_tags( [ 'some-type' ] );
+
+		$this->assertArrayNotHasKey( 'property', $sut->add_field_args( 'some-type', $field_args )['js_vars'] );
+	}
+
+	/**
+	 * @test
+	 * it should not modify markup if tag not supported
+	 */
+	public function it_should_not_modify_markup_if_tag_not_supported() {
+		$field_args = [ 'settings' => 'value' ];
+		$sut        = $this->make_instance();
+
+		$sut->set_wrapping_tag( 'span' );
+		$this->assertEquals( 'some-output', $sut->modify_output( 'unsupported', $field_args, 'some-output' ) );
+	}
+
+	/**
+	 * @test
+	 * it should wrap markup with before and after is supported
+	 */
+	public function it_should_wrap_markup_with_before_and_after_is_supported() {
+		$field_args = [ 'settings' => 'value' ];
+		$sut        = $this->make_instance();
+		$sut->set_supported_tags( [ 'some-type' ] );
+		$sut->set_markup_mods( [ 'some-type' => [ 'before' => 'before-', 'after' => '-after' ] ] );
+
+		$this->assertEquals( 'before-foo-after', $sut->modify_output( 'some-type', $field_args, 'foo' ) );
+	}
+
+	/**
+	 * @test
+	 * it should call the markup modifying callback if supported
+	 */
+	public function it_should_call_the_markup_modifying_callback_if_supported() {
+		$field_args = [ 'settings' => 'value' ];
+		$sut        = $this->make_instance();
+		$sut->set_supported_tags( [ 'some-type' ] );
+		$callback = function ( $tag, array $field_args, $output ) {
+			return 'before-' . $output . '-after';
+		};
+		$sut->set_markup_mods( [ 'some-type' => [ 'callback' => $callback ] ] );
+
+		$this->assertEquals( 'before-foo-after', $sut->modify_output( 'some-type', $field_args, 'foo' ) );
+	}
+
 	public function tags_and_fields() {
 		$title = $this->default_title_field_args();
 
@@ -37,17 +113,7 @@ class PostMessageTransportTest extends RefreshTransportTest {
 				'title',
 				$title,
 				array_merge( $title,
-					array(
-						'transport' => 'postMessage',
-						'js_vars'   => array(
-							array(
-								'element'  => '.some-setting',
-								'function' => 'html',
-								'prefix'   => '<span class=".some-setting" style="display: inline;">',
-								'suffix'   => '</span>'
-							)
-						)
-					) )
+					$this->additional_args() )
 			],
 		];
 	}
@@ -84,6 +150,9 @@ class PostMessageTransportTest extends RefreshTransportTest {
 		$this->assertEquals( $expected_output, $sut->modify_output( $tag, $field_args, $markup ) );
 	}
 
+	/**
+	 * @return \FTB_Fields_PostMessageTransport
+	 */
 	private function make_instance() {
 		return new \FTB_Fields_PostMessageTransport();
 	}
@@ -93,5 +162,22 @@ class PostMessageTransportTest extends RefreshTransportTest {
 	 */
 	private function default_title_field_args() {
 		return array( 'type' => 'text', 'settings' => 'some-setting', 'section' => 'some-section', 'default' => 'Some Value' );
+	}
+
+	/**
+	 * @return array
+	 */
+	private function additional_args() {
+		return array(
+			'transport' => 'postMessage',
+			'js_vars'   => array(
+				array(
+					'element'  => '.some-setting',
+					'function' => 'html',
+					'prefix'   => '<span class=".some-setting" style="display: inline;">',
+					'suffix'   => '</span>'
+				)
+			)
+		);
 	}
 }
