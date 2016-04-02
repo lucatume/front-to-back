26861abedc1f0c33a9eb463a44922c40fc1843df
Luca Tumedei <luca@theaveragedev.com>
2016-04-02 19:19:50 +0200

    fixed php parsing issues

--- src/FTB/Templates/Preprocessor.php
+++ src/FTB/Templates/Preprocessor.php
@@ -4,10 +4,83 @@
 class FTB_Templates_Preprocessor implements FTB_Templates_PreprocessorInterface {
 
 	/**
+	 * @var bool
+	 */
+	protected $in_attr;
+
+	/**
+	 * @var bool
+	 */
+	protected $context_locked = false;
+
+	/**
+	 * @var bool
+	 */
+	protected $in_tag = false;
+
+	/**
 	 * @param string $template_contents
 	 *
 	 * @return string
 	 */
 	public function neuter_php_tags( $template_contents ) {
+		$tokens = token_get_all( $template_contents );
+
+		$output_frags = array_map( array( $this, 'replace_php_tag' ), $tokens );
+
+		return join( '', $output_frags );
+	}
+
+	protected function replace_php_tag( $token ) {
+		if ( is_array( $token ) ) {
+			list( $index, $code, $line ) = $token;
+			if ( ! ( $this->context_locked || in_array( $index, $this->replaced_php_tags() ) ) ) {
+				$this->in_attr = preg_match( '/=\\s*("|\')\\s*$/', $code );
+				$this->in_tag  = ! $this->in_attr && ! preg_match( '/>\\s*$/', $code );
+			}
+			switch ( $index ) {
+				case T_OPEN_TAG:
+					return $this->replace_open_tag();
+				case T_CLOSE_TAG:
+					return $this->replace_close_tag();
+				default:
+					return $code;
+			}
+		}
+
+		return $token;
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function replace_open_tag() {
+		$this->context_locked = true;
+
+		$out = $this->in_tag ? 'data-ftb-php=" ' : '<!--?php ';
+
+		return $out;
+
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function replace_close_tag() {
+		$out = $this->in_tag ? '"' : '?-->';
+
+		$this->reset_context();
+
+		return $out;
+	}
+
+	private function replaced_php_tags() {
+		return array( T_OPEN_TAG, T_CLOSE_TAG );
+	}
+
+	protected function reset_context() {
+		$this->context_locked = false;
+		$this->in_attr        = false;
+		$this->in_tag         = false;
 	}
 }
--- tests/wpunit/FTB/Templates/PreprocessorTest.php
+++ tests/wpunit/FTB/Templates/PreprocessorTest.php
@@ -27,10 +27,150 @@ class PreprocessorTest extends \Codeception\TestCase\WPTestCase {
 		$this->assertInstanceOf( 'FTB_Templates_Preprocessor', $sut );
 	}
 
+	/**
+	 * @test
+	 * it should not modify a string not containing php tags
+	 */
+	public function it_should_not_modify_a_string_not_containing_php_tags() {
+		$in  = <<<HTML
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Document</title>
+</head>
+<body>
 
+</body>
+</html>
+HTML;
+		$sut = $this->make_instance();
+
+		$out = $sut->neuter_php_tags( $in );
+
+		$this->assertHtmlEquals( $in, $out );
+	}
+
+	/**
+	 * @test
+	 * it should neuter php tags in html code
+	 */
+	public function it_should_neuter_php_tags_in_html_code() {
+		$in = <<<HTML
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Document</title>
+</head>
+<body>
+<p><?php f(); ?></p>
+</body>
+</html>
+HTML;
+
+		$expected = <<<HTML
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Document</title>
+</head>
+<body>
+<p><!--?php f(); ?--></p>
+</body>
+</html>
+HTML;
+		$sut      = $this->make_instance();
+
+		$out = $sut->neuter_php_tags( $in );
+
+		$this->assertHtmlEquals( $expected, $out );
+	}
+
+	/**
+	 * @test
+	 * it should replace php tags in attributes
+	 */
+	public function it_should_replace_php_tags_in_attributes() {
+		$in = <<<HTML
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Document</title>
+</head>
+<body>
+<p class="<?php f(); ?>"></p>
+</body>
+</html>
+HTML;
+
+		$expected = <<<HTML
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Document</title>
+</head>
+<body>
+<p class="<!--?php f(); ?-->"></p>
+</body>
+</html>
+HTML;
+		$sut      = $this->make_instance();
+
+		$out = $sut->neuter_php_tags( $in );
+
+		$this->assertHtmlEquals( $expected, $out );
+	}
+
+	/**
+	 * @test
+	 * it should replace php tags in html tags
+	 */
+	public function it_should_replace_php_tags_in_html_tags() {
+		$in = <<<HTML
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Document</title>
+</head>
+<body>
+<p <?php f(); ?>></p>
+</body>
+</html>
+HTML;
+
+		$expected = <<<HTML
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Document</title>
+</head>
+<body>
+<p data-ftb-php=" f(); "></p>
+</body>
+</html>
+HTML;
+		$sut      = $this->make_instance();
+
+		$out = $sut->neuter_php_tags( $in );
+
+		$this->assertHtmlEquals( $expected, $out );
+	}
 
 	private function make_instance() {
 		return new \FTB_Templates_Preprocessor();
+	}
+
+	private function assertHtmlEquals( $expected, $actual, $message = '' ) {
+		$expectedString = preg_replace( '/\\s/', ' ', $expected );
+		$actualString   = preg_replace( '/\\s/', ' ', $actual );
+
+		$this->assertEquals( $expectedString, $actualString, $message );
 	}
 
 }
